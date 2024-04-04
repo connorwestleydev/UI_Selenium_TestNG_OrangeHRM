ddc3b034ceedab550f33f22eb7fe0ed654b6409c
connorwestleydev <connorpeterwestley@gmail.com>
2024-04-04 14:28:57 +0100

    Add BaseTest class
    BaseTests includes setUp and tearDown methods for launching the browser to the AUT
    before each test class and closing the browser after each test class. A test method is
    included to check the setUp and tearDown methods work correctly.

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,30 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSetUpAndTearDown() {
+
+    }
+}
