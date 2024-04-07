247c9531fca4dff6eeb4b10edfdfb47a8ef8ee28
connorwestleydev <connorpeterwestley@gmail.com>
2024-04-07 15:08:46 +0100

    Create initial dashboard test
    Initial dashboard test created to interact with employee leave modal and confirm success message
    is displayed when employee leave config is changed. Locators and methods added to DashboardPage object.
    Empty test methods added for subsequent dashboard tests I plan to add.

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -8,6 +8,10 @@ public class DashboardPage extends BasePage {
     private By headerBreadcrumb = By.cssSelector(".oxd-topbar-header-breadcrumb-module");
     private By userDropdown = By.cssSelector(".oxd-userdropdown");
     private By logoutLink = By.linkText("Logout");
+    private By employeeLeaveCogIcon = By.cssSelector(".orangehrm-leave-card-icon");
+    private By configModalSwitchToggle = By.cssSelector(".orangehrm-dialog-modal .oxd-switch-input");
+    private By configModalSaveButton = By.cssSelector(".orangehrm-dialog-modal button[type=submit]");
+    private By successNotification = By.cssSelector("#oxd-toaster_1 .oxd-toast--success");
 
     public DashboardPage(WebDriver driver) {
         super(driver);
@@ -24,5 +28,21 @@ public class DashboardPage extends BasePage {
     public void logout() {
         getElement(userDropdown).click();
         getElement(logoutLink).click();
+    }
+
+    public void clickEmployeeLeavingCogIcon() {
+        clickElement(employeeLeaveCogIcon);
+    }
+
+    public void clickConfigModalSwitchToggle() {
+        clickElement(configModalSwitchToggle);
+    }
+
+    public void clickConfigModalSaveButton() {
+        clickElement(configModalSaveButton);
+    }
+
+    public boolean isSuccessNotificationDisplayed() {
+        return getElement(successNotification).isDisplayed();
     }
 }
