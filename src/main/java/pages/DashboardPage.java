37fb76babd6b2c975eb1b8e63cc73dc35893d985
connorwestleydev <connorpeterwestley@gmail.com>
2024-04-08 14:37:57 +0100

    Add test clicking Buzz Post navigates to Buzz Page
    New dashboard test added to confirm that clicking the first buzz post
    shown on the dashboard page takes you to the Buzz Page within the app.
    BuzzPage page object class and DashboardPage methods add to assist with this.

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class DashboardPage extends BasePage {
 
@@ -13,6 +14,7 @@ public class DashboardPage extends BasePage {
     private By configModalSwitchToggle = By.cssSelector(".orangehrm-dialog-modal .oxd-switch-input");
     private By configModalSaveButton = By.cssSelector(".orangehrm-dialog-modal button[type=submit]");
     private By successNotification = By.cssSelector("#oxd-toaster_1 .oxd-toast--success");
+    private By buzzPosts = By.cssSelector(".orangehrm-buzz-widget-header-text");
 
     public DashboardPage(WebDriver driver) {
         super(driver);
@@ -60,5 +62,16 @@ public class DashboardPage extends BasePage {
         }
 
         return null;
+    }
+
+    /**
+     * Clicks the desired 'Buzz Post' in the Dashboard feed
+     * @param index starts at 1
+     * @return a new BuzzPage object
+     */
+    public BuzzPage clickBuzzPost(int index) {
+        WebElement buzzPost = getElements(buzzPosts).get(index - 1);
+        buzzPost.click();
+        return new BuzzPage(driver);
     }
 }
