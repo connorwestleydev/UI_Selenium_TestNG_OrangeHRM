82ec14b6f83e076623d4afdbc885e965f9456cdc
connorwestleydev <connorpeterwestley@gmail.com>
2024-04-08 11:39:41 +0100

    Add 'Help Button Opens Link In New Tab' test
    New test added to DashboardTests. BeforeMethod and AfterMethod methods added to dashboard tests
    to remove duplicate login/logout statements. Driver methods added to BasePage and DashboardPage
    and HelpPage object added to handle the required test actions (switching tabs).
    
    BaseTests annotations updated to make setUp and tearDown methods run before and after suite.
    Initialise LoginPage object moved to its own method so it is run before each test class

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,26 +2,29 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 import pages.LoginPage;
 
 public class BaseTests {
 
-    private WebDriver driver;
+    private static WebDriver driver;
     protected LoginPage loginPage;
 
-    @BeforeClass
-    public void setUp() {
+    @BeforeSuite
+    public static void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @BeforeClass
+    public void initialiseLogin() {
         loginPage = new LoginPage(driver);
     }
 
-    @AfterClass
-    public void tearDown() {
+    @AfterSuite
+    public static void tearDown() {
         driver.quit();
     }
 }
--- src/test/java/dashboard/DashboardTests.java
+++ src/test/java/dashboard/DashboardTests.java
@@ -1,32 +1,49 @@
 package dashboard;
 
 import base.BaseTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
+import pages.HelpPage;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class DashboardTests extends BaseTests {
 
+    private DashboardPage dashboardPage;
+
+    @BeforeMethod
+    public void login() {
+        this.loginPage.enterCredentials("admin", "admin123");
+        this.dashboardPage = loginPage.clickLoginButton();
+    }
+
+    @AfterMethod
+    public void logout() {
+        dashboardPage.logout();
+    }
+
     @Test
     public void testEmployeeLeaveConfigChange() {
-        loginPage.enterCredentials("admin", "admin123");
-        DashboardPage dashboardPage = loginPage.clickLoginButton();
-
         dashboardPage.clickEmployeeLeavingCogIcon();
         dashboardPage.clickConfigModalSwitchToggle();
         dashboardPage.clickConfigModalSaveButton();
-
         assertTrue(dashboardPage.isSuccessNotificationDisplayed(), "Success notification not displayed");
     }
 
     @Test
     public void testHelpButtonOpensLinkInNewTab() {
-
+        dashboardPage.clickHelpButton();
+        assertEquals(dashboardPage.getTabCount(), 2,"Two tabs are not open");
+        HelpPage helpPage = dashboardPage.switchToHelpPageTab();
+        assertTrue(helpPage.isAdminGuideLinkDisplayed(), "Admin Guide link is not visible");
+        dashboardPage = helpPage.switchToDashboardPageTab();
     }
 
-    @Test
-    public void testEmployeeByDeptChartShowsTooltipOnHover() {
-
-    }
+//    @Test
+//    public void testEmployeeByDeptChartShowsTooltipOnHover() {
+//
+//    }
 }
